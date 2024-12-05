d12aa4d416064fce32865f7adeb367dc8a9684d9
Mazen Kamel <107046923+mazenwk@users.noreply.github.com>
2024-12-05 20:35:26 +0200

    implement initial odometry environment aligner

---
+++ 3D_construction/3d_reconstruction.py
@@ -0,0 +1,17 @@
+﻿import sys
+import os
+
+from modules.odometry_aligner import PointCloudOdometryAligner, AlignDirection
+
+def main():
+    oaligner = PointCloudOdometryAligner(
+        scenario_path=r'S:\researchlab\auc-research\Pedistrian-crossing-prediction\LOKI',
+        loki_csv_path=r'S:\researchlab\auc-research\Pedistrian-crossing-prediction\LOKI\loki.csv',
+        key_frame=30
+    ) 
+    
+    oaligner.align(30, 10, AlignDirection.SPLIT)
+
+if __name__ == '__main__':
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+    main()
---
+++ 3D_construction/modules/utils/logger.py
@@ -0,0 +1,41 @@
+﻿# modules/logger.py
+import logging
+
+
+class ColorFormatter(logging.Formatter):
+    LEVEL_COLORS = {
+        logging.DEBUG: "\033[92m",  # Green
+        logging.INFO: "\033[94m",  # Blue
+        logging.WARNING: "\033[93m",  # Yellow
+        logging.ERROR: "\033[91m",  # Red
+        logging.CRITICAL: "\033[1;91m"  # Bright Red
+    }
+    RESET_COLOR = "\033[0m"
+
+    def format(self, record):
+        level_color = self.LEVEL_COLORS.get(record.levelno, self.RESET_COLOR)
+        message = super().format(record)
+        return f"{level_color}{message}{self.RESET_COLOR}"
+
+
+class Logger:
+    @staticmethod
+    def get_logger(name: str) -> logging.Logger:
+        """
+        Configure and return a logger with the specified name.
+    
+        Args:
+            name (str): Name of the logger.
+    
+        Returns:
+            logging.Logger: Configured logger.
+        """
+        logger = logging.getLogger(name)
+        if not logger.handlers:
+            handler = logging.StreamHandler()
+            formatter = ColorFormatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+            handler.setFormatter(formatter)
+            logger.setLevel(logging.DEBUG)
+            logger.addHandler(handler)
+            logger.propagate = False
+        return logger
