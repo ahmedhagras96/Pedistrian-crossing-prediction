5945cfd7c0f1bf27494ad0c5019bdf75ade61c09
Mazen Kamel <mazenwkamel@aucegypt.edu>
2025-01-09 23:29:00 +0200

    refactoring

--- src/scripts/source_path.py
+++ src/scripts/source_path.py
@@ -1,12 +1,16 @@
 ﻿import sys
 import os
 
-# Add the src directory to sys.path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-src_path = os.path.dirname(os.path.join(current_dir))
-modules_path = os.path.join(src_path, "modules")
-sys.path.insert(0, src_path)
-sys.path.insert(0, modules_path)
+def add_source_path():
+  # Add the src directory to sys.path
+  current_dir = os.path.dirname(os.path.abspath(__file__))
+  src_path = os.path.dirname(os.path.join(current_dir))
+  modules_path = os.path.join(src_path, "modules")
+  sys.path.insert(0, src_path)
+  sys.path.insert(0, modules_path)
 
-# Optional: Print sys.path to verify
-print(f"sys.path: {sys.path[0]}")
+  # Optional: Print sys.path to verify
+  print(f"sys.path: {sys.path}")
+
+
+add_source_path()
