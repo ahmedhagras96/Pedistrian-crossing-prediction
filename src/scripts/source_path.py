b69da6f1e13200a7764992061e13b1e4f15bc97f
Mazen Kamel <mazenwkamel@aucegypt.edu>
2025-01-09 23:27:37 +0200

    update source path

--- src/scripts/source_path.py
+++ src/scripts/source_path.py
@@ -3,8 +3,10 @@ import os
 
 # Add the src directory to sys.path
 current_dir = os.path.dirname(os.path.abspath(__file__))
-src_path = os.path.join(current_dir, "..", "..")
+src_path = os.path.dirname(os.path.join(current_dir))
+modules_path = os.path.join(src_path, "modules")
 sys.path.insert(0, src_path)
+sys.path.insert(0, modules_path)
 
 # Optional: Print sys.path to verify
-# print(f"sys.path: {sys.path}")
+print(f"sys.path: {sys.path[0]}")
